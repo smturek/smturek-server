d749c6b6198e276b7762bd73e9eeeb61efb0c0cd
smturek <smturek@gmail.com>
2015-02-26 14:42:10 -0700

    returns scores json

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,8 @@
+class ScoresController < ApplicationController
+
+  def index
+    @scores = Score.all
+    render json: @scores
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Score.delete_all
+
+20.times do |i|
+  Score.create!(
+    score: (i + 1) * 10,
+    name: "Player #{i + 1}"
+  )
+end
