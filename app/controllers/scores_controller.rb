ee43b777423a5e0a0a1fe9b772e62e3c7819f3ac
smturek <smturek@gmail.com>
2015-02-27 17:34:00 -0700

    create method

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -5,4 +5,16 @@ class ScoresController < ApplicationController
     render json: @scores
   end
 
+  def create
+    @score = Score.new(score_params)
+    @score.save()
+    rander json: @score
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:name, :score)
+  end
+
 end
